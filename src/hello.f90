4e635d5c17e821272a0d7f0fbfc507df755a3938
James Allsopp (Advanced Research Computing) <j.allsopp@bham.ac.uk>
2023-07-31 12:49:01 +0100

    Added a function to src/hello.f90, SumCustom to try and test the other version of testing

--- src/hello.f90
+++ src/hello.f90
@@ -3,8 +3,7 @@ PROGRAM hello
     use types_mod
     use date_mod, only : currentYear, currentDayOfYear
     use  solar_functions_mod    
-
-    real(kind=DP) :: theta, pi
+    real(kind=DP) :: theta, pi,test
 
     pi = 4.0_DP * atan( 1.0_DP )
     test = 123.0_DP
@@ -15,5 +14,12 @@ PROGRAM hello
     ! This is a comment line; it is ignored by the compiler
     print *, 'Hello, World!'
     print *, theta   
- !   theta = calcTheta()
+    print *, SumCustom(2,5,9)
+  CONTAINS
+    INTEGER FUNCTION SumCustom(x,y,z)
+      IMPLICIT NONE
+      INTEGER, INTENT(IN)::x,y,z
+      SumCustom = x+y+z
+    END FUNCTION SumCustom
+
 END PROGRAM hello
